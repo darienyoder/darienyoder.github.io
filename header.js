76a0a42a35286d2afd7d38b7a51f1456ffbba23f
darienyoder <116597751+darienyoder@users.noreply.github.com>
2024-08-01 16:55:05 -0400

    Moved portfolio items array to header.js

--- header.js
+++ header.js
@@ -1,3 +1,63 @@
+
+var portfolio_items =
+{
+	"Crossword Builder": {
+		name: "Crossword\nBuilder",
+		image: "crossword.png",
+		tags: ["code"],
+		link: "crossword-builder",
+	},
+	"Sandbox": {
+		name: "Sandbox",
+		image: "sandbox.png",
+		tags: ["code"],
+		link: "sandbox",
+	},
+	"SDL Chess": {
+		name: "SDL\nChess",
+		image: "chess.png",
+		tags: ["code"],
+		link: "sdl-chess",
+	},
+	"DestroyDog Bookmarklet": {
+		name: "DestroyDog\nBookmarklet",
+		image: "destroy-dog.png",
+		tags: ["code"],
+		link: "destroydog",
+	},
+	"Markdown Reader": {
+		name: "Markdown\nReader",
+		image: "markdown.png",
+		tags: ["code"],
+		link: "markdown-reader",
+	},
+
+    "Logo Design": {
+		name: "Logo\nDesign",
+		image: "graphic-design.png",
+		tags: ["graphic-design"],
+		link: "logo-design",
+	},
+	"TV Bumpers": {
+		name: "TV\nBumpers",
+		image: "tv-bumps.png",
+		tags: ["video"],
+		link: "tv-bumpers",
+	},
+	"Browser Games": {
+		name: "Browser\nGames",
+		image: "browsergames.png",
+		tags: ["code", "game"],
+		link: "games",
+	},
+	"Sketch Comedy": {
+		name: "Sketch\nComedy",
+		image: "sketch-comedy.png",
+		tags: ["video"],
+		link: "sketches",
+	},
+};
+
 function add_header()
 {
     let links = "";
